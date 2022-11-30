def7885f0e8409e44071984fe0b2139d52b76d55
tommy <wu798285@gmail.com>
2022-11-30 18:51:01 +0800

    search_shop.py final

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請傳送位置好幫您尋找附近的咖啡廳'))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='請傳送位置好幫您尋找附近的店家'))
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_message(event):
